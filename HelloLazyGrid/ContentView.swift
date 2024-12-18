46752e2b1d39977fad41dcc0d06ed87394272e9a
Willy <willy548798@gmail.com>
2024-12-18 17:45:52 +0800

    Initial Commit

---
+++ HelloLazyGrid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloLazyGrid
+//
+//  Created by Willy Hsu on 2024/12/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloLazyGrid/HelloLazyGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloLazyGridApp.swift
+//  HelloLazyGrid
+//
+//  Created by Willy Hsu on 2024/12/18.
+//
+
+import SwiftUI
+
+@main
+struct HelloLazyGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
